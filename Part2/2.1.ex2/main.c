324a7ae4bac32da69197737410d6fba177102c1b
ttan209 <totan95771@gmail.com>
2025-12-15 14:30:08 +0700

    Up bai 2 cua part 2

---
+++ Part2/2.1.ex2/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+double area (double a, double b, double c)
+{
+    double p = (a+b+c)/2;
+    double s = sqrt(p*(p-a)*(p-b)*(p-c));
+    return s;
+}
+int main()
+{
+    double a, b, c;
+    while(1)
+    {
+        scanf("%lf %lf %lf", &a, &b, &c);
+        if ((a+b>c) && (a+c>b) && (b+c>a) && a>=0 && b>=0 && c>=0)
+        {
+            printf("area is: %.2lf", area(a,b,c));
+            break;
+        }
+        else printf("Error value please re-enter value ! \n");
+    }
+    return 0;
+}
